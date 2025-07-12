eecac2f2eaa2e364af48de285b668eb29db0168e
Christine Wingfield <57592253+wingtek2020@users.noreply.github.com>
2025-07-12 10:18:54 -0400

    test

--- NotionsAPI/Controllers/WeatherForecastController.cs
+++ NotionsAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CNotionAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Test", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
